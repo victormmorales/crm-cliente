6334a300575ed0e8003903e434742d0e439832f6
victormmorales <victor.morales@criptan.com>
2022-07-26 12:53:36 +0200

    add normalize & tailwind

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,23 @@
+import Head from "next/head";
+import Sidebar from "./Sidebar";
+
 const Layout = ({ children }) => {
   return (
     <>
-      <h1>Desde layout</h1>
+      <Head>
+        <title>CRM - Administración de Clientes</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha256-l85OmPOjvil/SOvVt3HnSSjzF1TUMyT9eV0c2BzEGzU="
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Sidebar />
       {children}
     </>
   );
